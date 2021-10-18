c170d355405d442bc7821b282f269ffe52f26404
Maxime Dupuis <dupuismaxime@hotmail.com>
2021-10-17 23:00:14 -0400

    Setup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Stars } from "drei";
+import { Physics, usePlane, useBox } from "use-cannon";
+import "./styles.css";
 
-function App() {
+function Box() {
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh
+      onClick={() => {
+        api.velocity.set(0, 10, 0);
+      }}
+      ref={ref}
+      position={[0, 1, 0]}
+    >
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
   );
 }
 
-export default App;
+function Plane() {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+  }));
+  return (
+    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightgray" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <Canvas colorManagement camera={{ position: [-5, 2 - 10], fov: 60 }}>
+      <OrbitControls />
+      <Stars />
+      <directionalLight
+        position={[0, 10, 0]}
+        intensity={1.5}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+      />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Box />
+        <Plane />
+      </Physics>
+    </Canvas>
+  );
+}
